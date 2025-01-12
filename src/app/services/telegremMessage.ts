96e05db96f742a7ee28cb18e29a5275a1c80d95c
Igor-Konyaga <konyaga.igor10@gmail.com>
2025-01-12 22:50:12 +0200

    change env

--- src/app/services/telegremMessage.ts
+++ src/app/services/telegremMessage.ts
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
-
-const URL_API_TG = `https://api.telegram.org/bot${process.env.TOKEN}/sendMessage`;
+const URL_API_TG = `https://api.telegram.org/bot/${process.env.NEXT_PUBLIC_TOKEN}/sendMessage`;
 
 export const fetchTelegramMessage = async (message: string) => {
   const data = await axios.post(URL_API_TG, {
-    chat_id: process.env.CHAT_ID,
+    chat_id: process.env.NEXT_PUBLIC_CHAT_ID,
     text: message,
   });
 
