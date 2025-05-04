ba92455d26e9632502c2e550a002bd018b47ab2f
Igor-Konyaga <konyaga.igor10@gmail.com>
2025-05-04 12:35:14 +0300

    fix: update animation accordion

--- src/components/FAQ/Item/Item.tsx
+++ src/components/FAQ/Item/Item.tsx
@@ -1,11 +1,21 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { TObj as Props } from '../FAQ';
 import styles from './Item.module.scss';
 import { ArrowIcon } from '../../CustomIcons/ArrowIcon/ArrowIcon';
 
 export const Item = ({ title, desc }: Props) => {
   const [open, setOpen] = useState<boolean>(false);
+
+  const [height, setHeight] = useState(0);
+
+  const contentRef = useRef<HTMLParagraphElement | null>(null);
+
+  useEffect(() => {
+    if (contentRef.current) {
+      setHeight(contentRef.current.scrollHeight);
+    }
+  }, []);
 
   return (
     <li className={styles.item}>
@@ -14,10 +24,16 @@ export const Item = ({ title, desc }: Props) => {
         className={styles.btn}
         type="button"
       >
-          <span className={styles.title}>{title}</span>
+        <span className={styles.title}>{title}</span>
         <ArrowIcon active={open} className={styles.icon} />
       </button>
-      <p className={open ? `${styles.desc} ${styles.open}` : styles.desc}>
+      <p
+        ref={contentRef}
+        style={{
+          height: open ? `${height}px` : '0px',
+        }}
+        className={open ? `${styles.desc} ${styles.open}` : styles.desc}
+      >
         {desc}
       </p>
     </li>
